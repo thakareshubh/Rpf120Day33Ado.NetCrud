1b35d6498389960f1343484f4712d4420e656ef7
thakareshubh <shubhamthakare032@gmail.com>
2022-04-10 07:33:21 +0530

    create connection and retrieve data

---
+++ Uc1ConnectToDataBase/Uc1ConnectToDataBase/EmployeeRepo.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Uc1ConnectToDataBase
+{
+    class EmployeeRepo
+    {
+        public static string connectionString = @"(localdb)\MSSQLLocalDB;Database=payroll_service;Trusted_Connection=True";
+        SqlConnection connection = new SqlConnection(connectionString);
+        public void GetAllEmployee()
+        {
+            try
+            {
+                EmployeeModel employeeModel = new EmployeeModel();
+                using (this.connection)
+                {
+                    string query = @"SELECT EmployeeID,EmployeeName,PhoneNumber,Address,Department,Gender,BasicPay,Deductions,TaxablePay,Tax,NetPay,StartDate,City,Country
+                                    FROM Employee_payroll";
+
+                    //Define Sql Command Object
+                    SqlCommand cmd = new SqlCommand(query, this.connection);
+
+                    this.connection.Open();
+
+                    SqlDataReader dr = cmd.ExecuteReader();
+
+                    //check if there are records
+
+                    if (dr.HasRows)
+                    {
+                        while (dr.Read())
+                        {
+                            employeeModel.EmployeeID = dr.GetInt32(0);
+                            employeeModel.EmployeeName = dr.GetString(1);
+                            employeeModel.PhoneNumber = dr.GetString(2);
+                            employeeModel.Address = dr.GetString(3);
+                            employeeModel.Department = dr.GetString(4);
+                            employeeModel.Gender = Convert.ToChar(dr.GetString(5));
+                            employeeModel.BasicPay = dr.GetDouble(6);
+                            employeeModel.Deductions = dr.GetDouble(7);
+                            employeeModel.TaxablePay = dr.GetDouble(8);
+                            employeeModel.Tax = dr.GetDouble(9);
+                            employeeModel.NetPay = dr.GetDouble(10);
+                            employeeModel.StartDate = dr.GetDateTime(11);
+                            employeeModel.City = dr.GetString(12);
+                            employeeModel.Country = dr.GetString(13);
+
+                            //display retieved record
+
+                            Console.WriteLine("{0},{1},{2},{3},{4},{5}", employeeModel.EmployeeID, employeeModel.EmployeeName, employeeModel.PhoneNumber, employeeModel.Address, employeeModel.Department, employeeModel.Gender);
+                            Console.WriteLine("\n");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No Data Found");
+                    }
+                    //Close Data Reader
+                    dr.Close();
+                    this.connection.Close();
+
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                this.connection.Close();
+            }
+        }
+    }
+ }
---
+++ Uc1ConnectToDataBase/Uc1ConnectToDataBase/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Uc1ConnectToDataBase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Payroll Sql");
+
+            EmployeeRepo repo = new EmployeeRepo();
+            EmployeeModel employee = new EmployeeModel();
+
+            employee.EmployeeName = "Eren Jeager";
+            employee.PhoneNumber = "8106529025";
+            employee.Address = "25-4-710";
+            employee.Department = "Hr";
+            employee.Gender = 'M';
+            employee.BasicPay = 22000;  
+            employee.Deductions = 1500;
+            employee.TaxablePay = 200.00;
+            employee.Tax = 300;
+            employee.NetPay = 25000;
+            employee.StartDate = DateTime.Now;
+            employee.City = "Kazipet";
+            employee.Country = "India";
+            repo.GetAllEmployee();
+
+        }
+    }
+}
